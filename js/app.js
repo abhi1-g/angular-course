b224fc953760165fa1e11969e281f0f2e1908f19
Abhi Govula <abhi.govula@scriptbees.com>
2016-07-26 08:42:03 +0530

    Day 2

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function($scope) {
+    $scope.test = "New value";
+    var d=2;
+    $scope.users = [{value: "name 1"},{value: "name 2"},{value: "name 3"},{value: "name 4"},{value: "name 5"}, {value: "ll"}, {value: "kjhb"}, {value: "aaaaa"}, {value: "zzzzzzzzz"}, {value: "jjjjj"}];
+})
